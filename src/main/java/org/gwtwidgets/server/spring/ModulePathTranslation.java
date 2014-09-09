ad341fe046071ff7253a951812fb885cabe66c71
ggeorgovassilis <g.georgovassilis@gmail.com>
2014-09-09 18:17:49 +0300

    cleaning repo

--- src/main/java/org/gwtwidgets/server/spring/ModulePathTranslation.java
+++
@@ -1,8 +0,0 @@
-package org.gwtwidgets.server.spring;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface ModulePathTranslation {
-
-	String computeModuleBaseURL(HttpServletRequest request, String moduleBaseURL, String strongName);
-}
